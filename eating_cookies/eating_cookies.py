1e9e57a763bebfeac613aaf373c062aaad4a37e6
marie Hotchkiss <mtknowlton67@gmail.com>
2021-02-10 19:35:05 -0800

    made changes

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -19,6 +19,6 @@ def eating_cookies(n, cache = None):
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 10
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
